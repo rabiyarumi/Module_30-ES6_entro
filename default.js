212d2b4795f69e83cb0ea9bef7ad2a94c629b719
rabiyarumi <rabiyaarumi@gmail.com>
2024-09-28 11:50:08 +0600

    added arrow of es6

--- default.js
+++ default.js
@@ -1,23 +1,37 @@
 // default --> if value is not provided, take this as a default.
+function add(num1, num2){
+    const result = num1 + num2 ;
+    console.log(num1, num2, result);
+    return  result;
+}
+// const sum = add(5, 7);
+// const sum = add(5);
+
+//here the number2 value will be undefined and NaN
+const sum = add(5);
+const sum2 = add();
+
+// default --> if value is not provided, take this as a default.
 function add(num1 = 99, num2 = 0){
     const result = num1 + num2 + num3;
     console.log(num1, num2, result);
     return  result;
 }
 
-// const sum = add(5, 7);
-// const sum = add(5);
-const sum = add(5);
 
+//if we ecpect a string value than use empty string without gap as a defoult value
 function fullName(first, last =''){
     const full = first + ' ' + last;
     return full;
 }
 
+// /if we espect a array value than use empty string
 function friends(numbs = []){
 
 }
 
+// /if we ecpect an object value than use empty string
 function person(human = {}){
     
 }
+
---
+++ more-arrow.js
@@ -0,0 +1,32 @@
+const difference = (x, y) => x - y;
+const multiply = (first, second , third) => first * second * third;
+
+// single parameter or one parameter\\
+const getAge = (person) => person.age;
+// 1.getting object value
+const student = { name: 'ananata', age: 25}
+const age = getAge(student)
+console.log(age);
+
+
+// 2.getting index value of array
+const getThird = numbers => numbers[2];
+const third = getThird([5,9,35,2,13])
+console.log(third)
+
+// 3. senglep peramiter without barecat
+const doubleIt = num => num * 2;
+
+// no parameter
+const getPI = () => Math.PI
+console.log(getPI())
+
+// large arrow function. if you want to get anything returned from this function. then you have to use the return keyword  
+const doMath = (x, y, z) => {
+    const sum = x + y + z;
+    const mult = x * y * z;
+    const result = sum + mult
+    return result
+}
+
+console.log(doMath(5,4,6));
